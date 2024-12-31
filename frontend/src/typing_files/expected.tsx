310db5a8697e2da3cbfa89807308790bf2292589
gewoehnlich <oomyse@gmail.com>
2024-12-31 17:02:27 +0500

    some of the new typing functionality

---
+++ frontend/src/typing_files/expected.tsx
@@ -0,0 +1,34 @@
+export function handleExpectedKey(
+        currentWord: HTMLElement, 
+        currentLetter: HTMLElement, 
+        isLetter: boolean, 
+        isSpace: boolean
+    ): null {
+
+    if (isLetter) {
+        currentLetter.classList.remove("current");
+        currentLetter.classList.add("correct");
+        currentLetter.nextSibling.classList.add("current");
+    } 
+
+    else if (isSpace) {
+        let isWordWrong: boolean = false;
+        for (const letter of currentWord.childNodes) {
+            if (letter.classList.contains("incorrect")) {
+                isWordWrong = true;
+                break;
+            }
+        }
+
+        if (!isWordWrong) {
+            currentLetter.classList.remove("current");
+            currentWord.classList.remove("current");
+
+            const nextWord: HTMLDomElement = currentWord.nextSibling;
+            nextWord.classList.add("current");
+
+            const nextLetter: HTMLDomElement = nextWord.firstChild;
+            nextLetter.classList.add("current");
+        }
+    }
+}
