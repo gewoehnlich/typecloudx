9200f09e84e83ea5d9d0407a307a935647bca685
gewoehnlich <oomyse@gmail.com>
2024-12-31 17:03:30 +0500

    some of new typing functionality

--- frontend/src/typing.tsx
+++ frontend/src/typing.tsx
@@ -1,11 +1,39 @@
 import { useEffect } from "react";
+import { handleExpectedKey } from "./typing_files/expected.tsx";
 
 export const TypingComponent = () => {
     useEffect(() => {
         const handleKeydown = (ev: KeyboardEvent) => {
             console.log(ev.key);
+            const key: KeyboardEvent = ev.key;
+            const currentWord: HTMLDomElement = document.querySelector(".word.current");
+            const currentLetter: HTMLDomElement = currentWord.querySelector(".letter.current");
+            const expected: string = currentLetter.textContent || " ";
+            const isLetter: boolean = key.length === 1 && key != " ";
+            const isSpace: boolean = key === " ";
+            const isBackspace: boolean = key === 'Backspace';
 
-            console.log(document.getElementById("cursor"));
+            if (key === expected) {
+                handleExpectedKey(currentWord, currentLetter, isLetter, isSpace);
+            }
+
+            else if (isBackspace) {
+                const isFirstLetter: HTMLDomElement = currentLetter === currentWord.firstChild;
+                if (ev.ctrlKey) {
+                    const letters = [...document.querySelectorAll(".word.current .letter")];
+                    letters.forEach(letter => {
+                        letter.classList?.remove("incorrect");
+                        letter.classList?.remove("correct");
+                    });
+
+                    currentLetter.classList.remove("current");
+                    currentWord.firstChild.classList.add("current");
+                }
+
+                else if (!isFirstLetter) {
+                
+                }
+            }
         }
 
         document.addEventListener("keydown", handleKeydown);
